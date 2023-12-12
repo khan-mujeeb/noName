1b266db22be1fa0bd8309dc9c8508d2653730d62
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-12-12 20:43:39 +0530

    card style added

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,7 +1,7 @@
 const PageTitle = ({subTitle, title}) => {
     return (
         <div className="flex flex-col gap-1 items-center">
-            <p className="text-lg">{subTitle}</p>
+            <p className="text-lg text-gray-600">{subTitle}</p>
             <h2 className="text-5xl font-bold">{title}</h2>
         </div>
     );
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PageTitle from "./PageTitle";
 import projects from "../data/projectData";
+import { LuGithub } from "react-icons/lu";
 
 const ProjectSection = () => {
     const [type, setType] = useState("web");
@@ -25,24 +26,38 @@ const ProjectSection = () => {
             <div className="flex justify-center items-center flex-col">
                 <div className="grid grid-cols-2 gap-10">
                     {filteredProjects.map((project) => (
-                        <div key={project.id} className="w-[550px] p-10 rounded-xl shadow-md shadow-black">
+                        <div
+                            key={project.id}
+                            className="w-[550px] p-10 rounded-xl shadow-md shadow-gray-900 transition duration-300 hover:bg-stone-100"
+                        >
                             <div className="flex flex-col gap-4 items-center">
-                                <h1 className="text-2xl font-semibold">{project.name}</h1>
+                                <div className="flex justify-between items-center w-full">
+                                    <h1 className="text-2xl font-semibold text-gray-700 ">
+                                        {project.name}
+                                    </h1>
+
+                                    <a target="_blank" rel="noreferrer" href={project.github}>
+                                        <LuGithub/>
+                                    </a>
+                                </div>
+
                                 <iframe
-                                    width="90%"
-                                    height="275"
-                                    src= {project.youtube}
+                                    width="100%"
+                                    height="250"
+                                    src={project.youtube}
                                     title="YouTube video player"
                                     frameBorder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    allow=""
                                     allowfullscreen
                                 ></iframe>
-                                <p className=" text-sm">{project.description}</p>
+                                <p className=" text-sm text-gray-600">
+                                    {project.description}
+                                </p>
                                 <div className="flex gap-4">
                                     {project.skills.map((tag, tagId) => (
                                         <div
                                             key={tagId}
-                                            className="bg-gray-100 rounded-lg p-2 text-sm shadow-sm shadow-black"
+                                            className="bg-gray-100 rounded-lg p-2 text-sm shadow-sm shadow-gray-800 text-gray-600 hover:font-semibold transition duration-600 hover:bg-blue-300"
                                         >
                                             {tag}
                                         </div>
