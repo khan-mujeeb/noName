971dad3f75925c794593ff6553317ca17563c24f
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-12-15 02:11:04 +0530

    responsive project added

--- src/components/Projects/ProjectSection.jsx
+++ src/components/Projects/ProjectSection.jsx
@@ -18,7 +18,7 @@ const ProjectSection = () => {
 
     return (
         <div
-            className="select-none flex flex-col items-center mt-36"
+            className="select-none flex flex-col items-center mt-28"
         >
             <PageTitle title="Projects" subTitle="Browse My Recent" />
             <div className="flex gap-10 mt-10">
@@ -46,7 +46,7 @@ const ProjectSection = () => {
             </div>
 
             <div className="flex justify-center items-center flex-col mt-20">
-                <div className="grid grid-cols-2 gap-10">
+                <div className="grid xl:grid-cols-2 gap-10 grid-col-1">
                     {filteredProjects.map((project) => (
                         <ProjectListItem project={project} key={project.id} />
                     ))}
