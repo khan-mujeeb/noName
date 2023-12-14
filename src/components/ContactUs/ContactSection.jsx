253e42e4245c3ba3d0b2f0c95ebace95460d58c2
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-12-15 02:33:02 +0530

    responsive contact added

--- src/components/ContactUs/ContactSection.jsx
+++ src/components/ContactUs/ContactSection.jsx
@@ -11,11 +11,11 @@ import { CiSquareChevLeft } from "react-icons/ci";
 
 const ContactSection = () => {
     return (
-        <div  className="flex flex-col mt-36 ">
+        <div  className="flex flex-col mt-28 justify-center lg:max-2xl:gap-10 gap-5">
             <PageTitle title="Contact Me" subTitle={"say Hello"} />
-            <div className="flex justify-around items-center">
-                <div className="w-96 flex flex-col gap-4">
-                    <h1 className="text-5xl font-semibold ">
+            <div className="flex lg:max-2xl:justify-around justify-center items-center">
+                <div className="lg:max-2xl:w-96 flex flex-col gap-4 text-center">
+                    <h1 className="lg:max-2xl:text-5xl font-semibold text-3xl">
                         {"Let's Work Together"}
                     </h1>
                     <p>
@@ -23,9 +23,9 @@ const ContactSection = () => {
                         elit. Consectetur, adipisci repudiandae! Earum illum a,
                     </p>
                 </div>
-                <img className="w-96" src={contactMe} alt="" />
+                <img className="md:max-2xl:w-96 lg:max-2xl:inline-block hidden" src={contactMe} alt="" />
             </div>
-            <div className="flex items-center justify-around">
+            <div className="flex items-center justify-around lg:max-2xl:flex-row flex-col lg:max-2xl:gap-10 gap-5">
                 <div className="flex flex-col gap-5">
                     <InfoBlock
                         icon={<CiMail />}
@@ -37,15 +37,15 @@ const ContactSection = () => {
                 </div>
                 <div className="flex flex-col gap-5">
 
-                    <div className="flex p-2 w-96 border border-gray-600 rounded-xl items-center">
+                    <div className="flex p-2 lg:max-2xl:w-96 w-80 border border-gray-600 rounded-xl items-center">
                         <input className="flex-auto" type="text" placeholder="Enter Your Email"/>
                         <MdOutlineMail/>
                     </div>
-                    <div className="flex p-2 w-96 border border-gray-600 rounded-xl items-center">
+                    <div className="flex p-2 lg:max-2xl:w-96 w-80 border border-gray-600 rounded-xl items-center">
                         <input className="flex-auto" type="text" placeholder="Enter Your Subject"/>
                         <CiSquareChevLeft/>
                     </div>
-                    <div className="flex p-2 w-96 border h-36 border-gray-600 rounded-xl">
+                    <div className="flex p-2 lg:max-2xl:w-96 w-80 h-36 border-gray-600 rounded-xl">
                         <input className="w-full border-none" type="text" placeholder="Type Your Message"/>
                         <FiMessageSquare/>
                     </div>
