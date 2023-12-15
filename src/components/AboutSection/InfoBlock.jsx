95daa9e2a24c486ab22bc51677007ee9daa64813
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-12-15 16:17:59 +0530

    links updated

--- src/components/AboutSection/InfoBlock.jsx
+++ src/components/AboutSection/InfoBlock.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const InfoBlock = ({icon, data}) => {
   return (
-    <div className='flex gap-2 items-center backdrop-blur-sm'>
+    <div className=' text-gray-700 flex gap-2 items-center backdrop-blur-sm'>
         <div>{icon}</div>
         <div className=''>{data}</div>
     </div>
--- src/components/AboutSection/PersonalInfo.jsx
+++ src/components/AboutSection/PersonalInfo.jsx
@@ -4,32 +4,33 @@ import { FaPhone } from "react-icons/fa6";
 import { MdDateRange, MdMail } from "react-icons/md";
 import { IoIosSchool } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
+import contactInfo from "../../data/contactInfo.js";
 
 const PersonalInfo = () => {
     return <div className="flex flex-col w-full gap-10">
     {/* description  */}
-    <div>
-        {}
+    <div className=" text-gray-700">
+        {contactInfo.introduction}
     </div>
 
     {/* info  */}
     <div className="grid lg:max-2xl:grid-cols-2 gap-x-16 gap-y-2">
-        <InfoBlock icon={<FaUserAlt />} data="Mujeeb Khan" />
+        <InfoBlock icon={<FaUserAlt />} data= {contactInfo.name} />
 
-        <InfoBlock icon={<FaPhone />} data="+91 8766516237" />
+        <InfoBlock icon={<FaPhone />} data= {contactInfo.phoneNumber} />
 
-        <InfoBlock icon={<MdMail />} data="mujeebkhan1831@gmail.com" />
+        <InfoBlock icon={<MdMail />} data= {contactInfo.email} />
 
-        <InfoBlock icon={<MdDateRange />} data="18/02/2002" />
+        <InfoBlock icon={<MdDateRange />} data= {contactInfo.dob} />
 
-        <InfoBlock icon={<IoIosSchool />} data="B.E Information Technology" />
-        <InfoBlock icon={<FaLocationDot />} data="Pune, India" />
+        <InfoBlock icon={<IoIosSchool />} data= {contactInfo.education}/>
+        <InfoBlock icon={<FaLocationDot />} data= {contactInfo.location} />
     </div>
     {/* languages  */}
     <div className="flex flex-col gap-1">
         <h2 className=" font-semibold">Language Skills</h2>
         <div className="w-full h-[0.1px]  bg-gray-300"></div>
-        <p>English, Hindi, Marathi</p>
+        <p className="text-gray-700">English, Hindi, Marathi</p>
     </div>
 </div>
 };
